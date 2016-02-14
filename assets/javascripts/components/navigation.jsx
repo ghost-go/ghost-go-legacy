e99ec56116f55e60ee6130854a3baef390fd5880
happybai <happybyronbai@gmail.com>
2016-02-14 14:58:25 +0800

    add ease-in effect

--- assets/javascripts/components/navigation.jsx
+++ assets/javascripts/components/navigation.jsx
@@ -8,5 +8,18 @@ ReactDOM.render(
     <header className="nav-header">
       <a className="logo"></a>
     </header>
+
+    <section className="nav-body">
+      <div className="items clearfix">
+        <a href="#">Games</a>
+        <a href="#">Puzzles</a>
+        <a href="#">Me</a>
+        <a href="#">Help</a>
+      </div>
+    </section>
+
+    <footer className="nav-footer">
+
+    </footer>
   </div>
 , document.querySelector('.navigation-view'));
