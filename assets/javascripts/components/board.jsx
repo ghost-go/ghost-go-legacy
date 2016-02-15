353e855ea9ffe747078364a0ef0c094619be8ec2
happybai <happybyronbai@gmail.com>
2016-02-15 17:33:21 +0800

    add react-intl package and making intl

--- assets/javascripts/components/board.jsx
+++ assets/javascripts/components/board.jsx
@@ -2,6 +2,15 @@ import Board from '../board.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//class Board extends Component {
+  //constructor(props) {
+    //super(props);
+    //this.state = {
+
+    //}
+  //}
+//}
+
 let b = document.querySelector('.board-view')
 ReactDOM.render(
  <div className="board">
--- assets/javascripts/components/navigation.jsx
+++ assets/javascripts/components/navigation.jsx
@@ -1,34 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {IntlProvider, FormattedMessage, addLocaleData} from 'react-intl';
 
 require("../../stylesheets/navigation.scss");
 
+//if ('ReactIntlLocaleData' in window) {
+    //Object.keys(ReactIntlLocaleData).forEach((lang) => {
+        //addLocaleData(ReactIntlLocaleData[lang]);
+    //});
+//}
+
+class Navigation extends Component {
+
+  render() {
+    return (
+      <div className="nav-container">
+        <header className="nav-header">
+          <a className="logo"></a>
+        </header>
+
+        <section className="nav-body">
+          <div className="nav-body-wrap clearfix">
+            <a href="#">
+              <FormattedMessage
+                id="app.nav.menu.games"
+                defaultMessage="Games"
+              />
+            </a>
+            <a href="#">
+              <FormattedMessage
+                id='app.nav.menu.puzzles'
+                defaultMessage="Puzzles"
+              />
+            </a>
+            <a href="#">
+              <FormattedMessage
+                id='app.nav.menu.me'
+                defaultMessage="Me"
+              />
+            </a>
+            <a href="#">
+              <FormattedMessage
+                id='app.nav.menu.help'
+                defaultMessage="Help"
+              />
+            </a>
+          </div>
+        </section>
+
+        <footer className="nav-footer">
+          <div className="nav-footer-wrap">
+            <a href="#">
+              <i className="fa fa-search"></i>
+            </a>
+          </div>
+          <div className="nav-footer-wrap nav-question">
+            <a href="#">
+              <i className="fa fa-question-circle"></i>
+            </a>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+}
+
+const zhcnMessages = {
+  "app.nav.menu.games": "棋谱",
+  "app.nav.menu.puzzles": "死活题",
+  "app.nav.menu.me": "我的",
+  "app.nav.menu.help": "帮助",
+}
+
 ReactDOM.render(
-  <div className="nav-container">
-    <header className="nav-header">
-      <a className="logo"></a>
-    </header>
-
-    <section className="nav-body">
-      <div className="nav-body-wrap clearfix">
-        <a href="#">Games</a>
-        <a href="#">Puzzles</a>
-        <a href="#">Me</a>
-        <a href="#">Help</a>
-      </div>
-    </section>
-
-    <footer className="nav-footer">
-      <div className="nav-footer-wrap">
-        <a href="#">
-          <i className="fa fa-search"></i>
-        </a>
-      </div>
-      <div className="nav-footer-wrap nav-question">
-        <a href="#">
-          <i className="fa fa-question-circle"></i>
-        </a>
-      </div>
-    </footer>
-  </div>
+  <IntlProvider locale='zh-CN' messages={zhcnMessages}>
+    <Navigation />
+  </IntlProvider>
 , document.querySelector('.navigation-view'));
