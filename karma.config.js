0838c97b0fc12fc7e6223f244e5f940a33eefb00
happybai <happybyronbai@gmail.com>
2016-02-29 13:54:38 +0800

    upgrade webpack to 2.0

--- karma.config.js
+++ karma.config.js
@@ -6,9 +6,6 @@ module.exports = function (config) {
     files: [
       'tests/**/*_spec.js'
     ],
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
     plugins: [webpack, 'karma-jasmine', 'karma-chrome-launcher', 'karma-coverage', 'karma-spec-reporter'],
     browsers: [ 'Chrome' ],
     preprocessors: {
