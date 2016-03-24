c7c18f0d25b43ad672ef6895ec20e365767b04e0
happybai <happybyronbai@gmail.com>
2016-03-24 17:32:30 +0800

    make pagination

--- assets/javascripts/pages/Games.js
+++ assets/javascripts/pages/Games.js
@@ -4,16 +4,32 @@ import { IntlProvider, FormattedMessage, addLocaleData } from 'react-intl'
 import Board from '../presentations/Board'
 import lang from '../components/lang'
 import KifuTable from '../presentations/KifuTable'
+import Pagination from '../presentations/Pagination'
 import { connect } from 'react-redux'
 import { fetchKifus } from '../actions/KifuActions'
+import { Link } from 'react-router';
+import { Router, Route, hashHistory, browserHistory } from 'react-router'
 
 class Games extends Component {
   constructor(props) {
     super(props)
-    this.props.dispatch(fetchKifus({
-      page: 1,
-      per_page: 30
-    }))
+    this.state = {
+      total: 11,
+      current: 0,
+      visablePage: 6
+    }
+    let { query } = this.props.location
+    console.log(this.state.visablePage);
+    this.props.dispatch(fetchKifus(query.page, query.per_page))
+
+    this.handlePageChanged = this.handlePageChanged.bind(this)
+  }
+
+  handlePageChanged(newPage) {
+    this.setState({ current: newPage }, () => {
+      console.log(this.state)
+      this.props.dispatch(fetchKifus(this.state.current + 1, 30))
+    })
   }
 
   render() {
@@ -29,6 +45,19 @@ class Games extends Component {
               />
           </h1>
           <KifuTable kifus={ kifus.data } />
+          <Pagination current={this.state.current}
+                      total={this.state.total}
+                      visiblePages={this.state.visablePage}
+                      onPageChanged={this.handlePageChanged}
+                      titles = {{
+                        first: "First",
+                        prev: "Prev",
+                        prevSet: "<<<",
+                        nextSet: ">>>",
+                        next: "Next",
+                        last: "Last",
+                      }}
+          />
           <Board className="board" grid="19" size="30" />
         </div>
       </IntlProvider>
