175f8754246750cfd18a67ec223e6419b692deaa
happybai <happybyronbai@gmail.com>
2016-05-11 16:54:47 +0800

    Fix production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -4,7 +4,7 @@ var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 
 module.exports = {
-  entry: './index.jsx',
+  entry: './index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
