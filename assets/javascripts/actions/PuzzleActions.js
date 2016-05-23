721f756b8787071d567767d47e6db3e1a27a7aa3
happybai <happybyronbai@gmail.com>
2016-05-23 12:40:28 +0800

    Make puzzle action

---
+++ assets/javascripts/actions/PuzzleActions.js
@@ -0,0 +1,38 @@
+import * as types from '../constants/ActionTypes'
+import { createAction, handleAction, handleActions } from 'redux-actions'
+
+export const fetchPuzzlesRequest = createAction(types.FETCH_PUZZLES_REQUEST)
+export const fetchPuzzlesSuccess = createAction(types.FETCH_PUZZLES_SUCCESS)
+export const fetchPuzzlesFailure = createAction(types.FETCH_PUZZLES_FAILURE)
+
+export const fetchPuzzleRequest = createAction(types.FETCH_PUZZLE_REQUEST)
+export const fetchPuzzleSuccess = createAction(types.FETCH_PUZZLE_SUCCESS)
+export const fetchPuzzleFailure = createAction(types.FETCH_PUZZLE_FAILURE)
+
+export function fetchPuzzles(page, per_page) {
+  return dispatch => {
+    dispatch(fetchPuzzlesRequest({page, per_page}))
+    //let url = `${API_AdDDRESS}/v1/kifu?page=1&per_page=10`;
+    let url = `http://api.ghost-go.com/v1/kifus?page=${page}`
+    if (per_page != null) {
+      url = `${url}&per_page=${per_page}`
+    }
+
+    return fetch(url)
+      .then(res => res.json())
+      .then(data => dispatch(fetchPuzzlesSuccess({page, per_page, data})))
+      .catch(ex => dispatch(fetchPuzzlesFailure(ex)))
+  }
+}
+
+export function fetchPuzzle(id) {
+  return dispatch => {
+    dispatch(fetchPuzzleRequest({id}))
+    let url = `http://api.ghost-go.com/v1/kifus/${id}`
+    return fetch(url)
+      .then(res => res.json())
+      .then(data => dispatch(fetchPuzzleSuccess({data})))
+      .catch(ex => dispatch(fetchPuzzleFailure(ex)))
+  }
+}
+
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -12,10 +12,6 @@ import Games from './pages/Games'
 import Kifus from './pages/Kifus'
 import Sign from './pages/Sign'
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-
 class App extends React.Component {
   render() {
     return (
