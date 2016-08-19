00228dee89f4478bf74f2b46d16c7130c33e7836
happybai <happybyronbai@gmail.com>
2016-08-19 16:38:58 +0800

    Update package.json and fix production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -43,7 +43,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'es2017']
         }
       },
       {
