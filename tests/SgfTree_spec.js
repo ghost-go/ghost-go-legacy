d44e8ddaa5cf7f1e25fa152e247847229d2402ec
happybai <happybyronbai@gmail.com>
2017-04-14 14:45:44 +0800

    Add specs

--- tests/SgfTree_spec.js
+++
@@ -1,6 +0,0 @@
-import SgfTree from '../assets/javascripts/eboard/SgfTree'
-
-describe('create a control bar instance and initial', () => {
-  it ('controlbar component should be initial', () => {
-  })
-})
