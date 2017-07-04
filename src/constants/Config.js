7d40b2606fe6b5e83ffb8a12c5ae68a163979534
happybai <happybyronbai@gmail.com>
2017-07-04 15:04:53 +0800

    Add total version

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -2,6 +2,7 @@ let protocol = 'http';
 let apiDomain = '';
 let appDomain = '';
 export const ENV = process.env.NODE_ENV;
+export const TOTAL_VERSION = process.env.TOTAL_VERSION;
 
 switch (ENV) {
   case 'development':
