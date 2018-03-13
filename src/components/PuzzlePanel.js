713317bb02dcb39c637f38a1aada7aa60bdf1dc6
happybai <byron.bai@aol.com>
2018-03-13 14:18:48 +0800

    Remove share buttons

--- src/components/PuzzlePanel.js
+++ src/components/PuzzlePanel.js
@@ -116,26 +116,6 @@ export default class PuzzlePanel extends Component {
   }
 
   render() {
-    const {
-      FacebookShareButton,
-      GooglePlusShareButton,
-      LinkedinShareButton,
-      TwitterShareButton,
-      PinterestShareButton,
-      VKShareButton,
-      TelegramShareButton,
-      WhatsappShareButton,
-    } = ShareButtons;
-
-    const FacebookIcon = generateShareIcon('facebook');
-    const TwitterIcon = generateShareIcon('twitter');
-    const GooglePlusIcon = generateShareIcon('google');
-    const LinkedinIcon = generateShareIcon('linkedin');
-    const PinterestIcon = generateShareIcon('pinterest');
-    const VKIcon = generateShareIcon('vk');
-    const TelegramIcon = generateShareIcon('telegram');
-    const WhatsappIcon = generateShareIcon('whatsapp');
-
     const { puzzle } = this.props;
     if (puzzle === undefined) return null;
 
@@ -226,109 +206,6 @@ export default class PuzzlePanel extends Component {
         </div>
         <div>
           { nextPanel }
-        </div>
-        <div className="share-container">
-          <div className="share-box">
-            <FacebookShareButton
-              url={shareUrl}
-              title={title}
-              picture={`${exampleImage}`}
-              className="share-button"
-            >
-              <FacebookIcon
-                size={32}
-                round
-              />
-            </FacebookShareButton>
-          </div>
-
-          <div className="share-box">
-            <TwitterShareButton
-              url={shareUrl}
-              title={title}
-              className="share-button"
-            >
-              <TwitterIcon
-                size={32}
-                round
-              />
-            </TwitterShareButton>
-          </div>
-
-          <div className="share-box">
-            <TelegramShareButton
-              url={shareUrl}
-              title={title}
-              className="share-button"
-            >
-              <TelegramIcon size={32} round />
-            </TelegramShareButton>
-          </div>
-
-          <div className="share-box">
-            <WhatsappShareButton
-              url={shareUrl}
-              title={title}
-              separator=":: "
-              className="share-button"
-            >
-              <WhatsappIcon size={32} round />
-            </WhatsappShareButton>
-          </div>
-
-          <div className="share-box">
-            <GooglePlusShareButton
-              url={shareUrl}
-              className="share-button"
-            >
-              <GooglePlusIcon
-                size={32}
-                round
-              />
-            </GooglePlusShareButton>
-          </div>
-
-          <div className="share-box">
-            <LinkedinShareButton
-              url={shareUrl}
-              title={title}
-              windowWidth={750}
-              windowHeight={600}
-              className="share-button"
-            >
-              <LinkedinIcon
-                size={32}
-                round
-              />
-            </LinkedinShareButton>
-          </div>
-
-          <div className="share-box">
-            <PinterestShareButton
-              url={String(window.location)}
-              media={`${exampleImage}`}
-              windowWidth={1000}
-              windowHeight={730}
-              className="share-button"
-            >
-              <PinterestIcon size={32} round />
-            </PinterestShareButton>
-          </div>
-
-          <div className="share-box">
-            <VKShareButton
-              url={shareUrl}
-              image={`${exampleImage}`}
-              windowWidth={660}
-              windowHeight={460}
-              className="share-button"
-            >
-              <VKIcon
-                size={32}
-                round
-              />
-            </VKShareButton>
-          </div>
         </div>
         <div className="clearfix" />
         <Toggle
