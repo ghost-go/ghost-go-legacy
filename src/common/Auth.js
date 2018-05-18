f4375abb9e131ed6e1c0a5b312f07bf34ab3466a
happybai <byron.bai@aol.com>
2018-05-18 19:45:44 +0800

    Hotfix

--- src/common/Auth.js
+++ src/common/Auth.js
@@ -63,7 +63,7 @@ export default class Auth {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
-    localStorage.removeItem('profile');
+    // localStorage.removeItem('profile');
     // navigate to the home route
     this.history.replace('/problems');
   }
